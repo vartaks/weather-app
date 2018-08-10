08407d3a5030d31be65fcccf597abc6131b389bc
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-08-10 18:29:06 +0530

    Added sample HTTP request call to the google maps geocode API

---
+++ app.js
@@ -0,0 +1,11 @@
+const request = require('request');
+
+request({
+    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    json: true
+}, (error, response, body) => {
+    // console.log(JSON.stringify(body, null, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});
