604a8ef3d9d278b072817704753849cf2dffdf37
Sourabh Vartak <vartak.sourabh@gmail.com>
2025-04-08 12:24:36 +0400

    update

--- playground/promise.js
+++ playground/promise.js
@@ -6,25 +6,25 @@ var asyncAdd = (a, b) => {
             } else {
                 reject('Arguments must be numbers');
             }
-        }, 1500);
+        }, 900);
     });
 };
 
-asyncAdd(5, 7).then((result) => {
+asyncAdd(5, 175).then((result) => {
     console.log('Result:', result);
 }, (errorMessage) => {
     console.log(errorMessage);
 });
 
-// var somePromise = new Promise((resolve, reject) => {
-//     setTimeout(() => {     
-//         //resolve('Hurray! It rocks!');
-//         reject('Unable to fulfill promise');
-//     }, 3000);
-// });
+var somePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {     
+        resolve('Hurray! It rocks!');
+        //reject('Unable to fulfill promise');
+    }, 3000);
+});
 
-// somePromise.then((message) => {
-//     console.log('Success:', message);
-// }, (errorMessage) => {
-//     console.log('Error:', errorMessage);
-// });
+somePromise.then((message) => {
+    console.log('Success:', message);
+}, (errorMessage) => {
+    console.log('Error:', errorMessage);
+});
